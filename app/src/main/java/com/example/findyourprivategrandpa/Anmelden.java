0c4f65f18b8516dbd49da27a89e791bbf958894c
Benedikt Oltersdorf <1810653774@stud.fh-kufstein.ac.at>
2020-01-26 14:11:40 +0100

    Design

---
+++ app/src/main/java/com/example/findyourprivategrandpa/Anmelden.java
@@ -0,0 +1,34 @@
+package com.example.findyourprivategrandpa;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Anmelden extends AppCompatActivity {
+
+    private Button button;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_anmelden);
+
+        button =findViewById(R.id.anmelden);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivitystart();
+            }
+        });
+
+    }
+
+    public void openActivitystart(){
+        Intent intent = new Intent(this,Start.class);
+        startActivity(intent);
+    }
+}
